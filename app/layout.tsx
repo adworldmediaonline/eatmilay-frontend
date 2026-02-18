f2ed3114f9fda24d0aa04f2a12c0f0f35fb164e9
fastestsunil <sunil141292@gmail.com>
2026-02-18 21:37:49 +0530

    Update RootLayout component: added a placeholder comment for future development, enhancing code clarity and maintainability.

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/*  */}
         <Providers>
           <Suspense fallback={<div className="flex min-h-svh items-center justify-center">Loading...</div>}>
             {children}
